a8e312944a5588c6e58ca3ae638ec9215104045f
anonymmed <mohamed.abdelhafidh94@gmail.com>
2021-01-19 22:16:05 +0200

    creatting pagination middleware

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/product');
+const middlewares = require('../utils/middlewares');
 
+router.get('/all', middlewares.pagination(Product), (req, res) => {
+   res.end(JSON.stringify(res.data), null, 3);
+});
 
 module.exports = router;
---
+++ backend/utils/middlewares.js
@@ -0,0 +1,49 @@
+module.exports = {
+    pagination: (model) => {
+        return async (req, res, next) => {
+
+            let page = parseInt(req.query.page);
+            if (page <= 0) {
+                res.status(500).json({ message: "Page cannot be less than 1" });
+            }
+
+            const limit = parseInt(req.query.limit);
+            const nbModels = await model.countDocuments().exec();
+            const maxPageNb = Math.ceil(nbModels / limit);
+
+            page = page > 0 ? page : 1;
+            page = page - 1 > maxPageNb ? 1 : page;
+
+            const start =  (page - 1) * limit ;
+            const end = page * limit;
+
+            const response = {};
+
+
+            if (end < nbModels) {
+                response.nextPage = page + 1;
+            }
+
+            if (start > 0) {
+                response.previousPage = page -1;
+            }
+            if (start >0 || end < nbModels) {
+                response.limit = limit;
+            }
+
+                if (limit >= nbModels || page -1 > maxPageNb) {
+                    response.data = model.find().exec().then((res) => res).catch((err) => {
+                        res.status(500).json({ message: err.message })
+                    })
+                } else {
+                    response.data = await model.find().limit(limit).skip(start).exec().then((res) => res)
+                        .catch((err) => {
+                            res.status(500).json({ message: err.message })
+                        })
+                }
+                res.data = response
+                next();
+        }
+
+    }
+}
